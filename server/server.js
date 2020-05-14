c6db1021db0918813444d8f719a7bd0ee32abf00
Richard Kazanowski <rjkaz@comcast.net>
2020-05-14 19:21:23 -0400

    update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
